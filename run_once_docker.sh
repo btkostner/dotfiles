0be751302928d4b2edf7b4b4cdb77bd67247a557
Blake Kostner <git@btkostner.io>
2020-05-05 23:06:54 -0600

    add docker-compose to docker install script

--- run_once_docker.sh
+++ run_once_docker.sh
@@ -24,3 +24,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
